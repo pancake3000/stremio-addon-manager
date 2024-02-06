10974d01f9402a973583931df9651dbbc4677e67
Sleeyax <yourd3veloper@gmail.com>
2024-02-06 22:16:56 +0100

    add link to source code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,14 @@
 </script>
 
 <template>
-  <div>
+  <div class="is-horizontal-align footer">
     <p>Made with ❤️, 🤖 and 🥞's - enjoy!</p>
+    <a href="https://github.com/pancake3000/stremio-addon-manager" target="_blank">Source Code</a>
   </div>
 </template>
 
 <style scoped>
+.footer {
+  gap: 5px;
+}
 </style>
